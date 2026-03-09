85baa271d97c7b4322ef62063bba9eda457eff7e
Ponomarev Roman <scv3lqqpup@gmail.com>
2026-03-09 19:08:04 +0300

    feat(nvim): add vim-tmux-navigator for seamless pane navigation

---
+++ nvim/lua/plugins/tmux-navigator.lua
@@ -0,0 +1,15 @@
+return {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+        "TmuxNavigateLeft",
+        "TmuxNavigateDown",
+        "TmuxNavigateUp",
+        "TmuxNavigateRight",
+    },
+    keys = {
+        { "<C-h>", "<cmd>TmuxNavigateLeft<CR>" },
+        { "<C-j>", "<cmd>TmuxNavigateDown<CR>" },
+        { "<C-k>", "<cmd>TmuxNavigateUp<CR>" },
+        { "<C-l>", "<cmd>TmuxNavigateRight<CR>" },
+    },
+}
