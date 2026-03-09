cdc156bca10d4c88a3ce08f069da0e81e27f34be
Ponomarev Roman <scv3lqqpup@gmail.com>
2026-03-09 19:24:49 +0300

    feat(zsh): add gl alias for git log with graph and body

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,6 +3,7 @@ alias n='nvim'
 alias lzd='lazydocker'
 alias gst='git status'
 alias gp='git push'
+alias gl='git log --graph --decorate --pretty=format:"%C(yellow)%h%C(reset) %C(auto)%d%C(reset) %s%n%w(0,4,4)%b"'
 
 # fzf
 alias nf='nvim $(fzf --preview "bat --color=always {}")'
