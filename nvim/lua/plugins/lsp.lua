3d0e27f044adc948aa6a0e8cf48ce965ade90150
Ponomarev Roman <scv3lqqpup@gmail.com>
2026-04-04 16:57:53 +0300

    fix(nvim): disable gopls usePlaceholders to prevent auto-inserting args

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -29,7 +29,7 @@ return {
                         analyses = { unusedparams = true },
                         staticcheck = true,
                         gofumpt = true,
-                        usePlaceholders = true,
+                        usePlaceholders = false,
                         completeUnimported = true,
                     },
                 },
