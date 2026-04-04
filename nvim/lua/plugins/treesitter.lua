d4cca82179886f1c0f0c4ca06afb0cee19beb6d3
Ponomarev Roman <scv3lqqpup@gmail.com>
2026-04-04 16:30:53 +0300

    revert(nvim): restore treesitter v0.9.3 with working config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,33 +1,35 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        tag = "v0.9.3",
         build = ":TSUpdate",
         config = function()
-            local langs = {
-                "go", "gomod", "gosum",
-                "python",
-                "lua",
-                "javascript", "typescript",
-                "json", "yaml", "toml",
-                "html", "css",
-                "bash",
-                "markdown", "markdown_inline",
-                "dockerfile",
-                "sql",
-                "vim", "vimdoc",
-            }
-
-            require("nvim-treesitter.install").install(langs)
-
-            vim.api.nvim_create_autocmd("FileType", {
-                callback = function(args)
-                    local ok = pcall(vim.treesitter.start, args.buf)
-                    if ok then
-                        vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-                    end
-                end,
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {
+                    "go", "gomod", "gosum",
+                    "python",
+                    "lua",
+                    "javascript", "typescript",
+                    "json", "yaml", "toml",
+                    "html", "css",
+                    "bash",
+                    "markdown", "markdown_inline",
+                    "dockerfile",
+                    "sql",
+                    "vim", "vimdoc",
+                },
+                highlight = { enable = true },
+                indent = { enable = true },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<C-space>",
+                        node_incremental = "<C-space>",
+                        scope_incremental = false,
+                        node_decremental = "<bs>",
+                    },
+                },
             })
-
             vim.treesitter.language.register("bash", "zsh")
         end,
     },
