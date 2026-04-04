828485893b6a8a66530bf925a6fe743d889ecd9a
Ponomarev Roman <scv3lqqpup@gmail.com>
2026-04-04 16:22:24 +0300

    fix(nvim): update treesitter and fix Go switch/case indentation

--- nvim/init.lua
+++ nvim/init.lua
@@ -44,6 +44,15 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" }
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Diagnostic float" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Diagnostic list" })
 
+-- Go indentation fix for switch/case
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "go",
+    callback = function()
+        vim.opt_local.cindent = false
+        vim.opt_local.autoindent = true
+    end,
+})
+
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
     callback = function() vim.highlight.on_yank() end,
